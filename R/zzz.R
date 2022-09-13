d5d48646cefdb8fe9bacee8ff832a8d52d7b6d9d
yurimaluf <yurimaluf@gmail.com>
2022-09-13 17:58:57 -0300

    teste

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+# .onLoad <- function(...){
+#   #msg <- paste("Loading", pkgname)
+#   cat("Em caso de algum bug por favor reportar \n") 
+# }
+
+.onAttach<-function(...){
+  cat("In case of a bug please report it to: yurimaluf@gmail.com \n")
+  #rstudioapi::showDialog( "BugReports","Report bugs at:", url="https://github.com/yurimaluf/RobustBetaReg/issues")
+}
